ffa3f87747b512f127cbf05c5d26d3d4d7630985
hafizhmuh <mhest01@gmail.com>
2022-06-17 19:44:39 +0700

    Add : File [S3-PBI14] ref #13

---
+++ spillaja/resources/views/halamanberitapasien.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.maindokter')
+<link rel="stylesheet" href="css/style.css">
+
+@section('container')
+    
+<section class="formulir" id="formulir">
+
+    <h1 class="heading"> Riwayat Laporan <span>#spillaja</span> </h1>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-10 mx-auto">
+                <div class="career-search mb-60">
+
+                    <div class="filter-result"> 
+                        <div class="job-box d-md-flex align-items-center justify-content-between mb-30">
+                            <div class="job-left my-4 d-md-flex flex-wrap">
+                                <div class="riwayat-content">
+                                    <h5 class="text-md-left">9 Cara Sederhana Menjaga Kesehatan Mental</h5>
+                                    <p>Amanda Priscilla | 07 Juni 2022</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus porttitor eros quis posuere facilisis. Etiam rhoncus porta odio, et cursus purus cursus eget. Vestibulum sit amet pretium magna. Ut viverra lobortis ante, ut gravida justo dignissim pellentesque. Quisque consectetur tincidunt odio quis condimentum. Vivamus eu turpis diam. Curabitur dignissim lacinia orci non placerat. Aliquam malesuada nunc eu tortor cursus, at auctor tellus viverra.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="filter-result"> 
+                        <div class="job-box d-md-flex align-items-center justify-content-between mb-30">
+                            <div class="job-left my-4 d-md-flex flex-wrap">
+                                <div class="riwayat-content">
+                                    <h5 class="text-md-left">9 Cara Sederhana Menjaga Kesehatan Mental</h5>
+                                    <p>Amanda Priscilla | 07 Juni 2022</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus porttitor eros quis posuere facilisis. Etiam rhoncus porta odio, et cursus purus cursus eget. Vestibulum sit amet pretium magna. Ut viverra lobortis ante, ut gravida justo dignissim pellentesque. Quisque consectetur tincidunt odio quis condimentum. Vivamus eu turpis diam. Curabitur dignissim lacinia orci non placerat. Aliquam malesuada nunc eu tortor cursus, at auctor tellus viverra.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="filter-result"> 
+                        <div class="job-box d-md-flex align-items-center justify-content-between mb-30">
+                            <div class="job-left my-4 d-md-flex flex-wrap">
+                                <div class="riwayat-content">
+                                    <h5 class="text-md-left">9 Cara Sederhana Menjaga Kesehatan Mental</h5>
+                                    <p>Amanda Priscilla | 07 Juni 2022</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus porttitor eros quis posuere facilisis. Etiam rhoncus porta odio, et cursus purus cursus eget. Vestibulum sit amet pretium magna. Ut viverra lobortis ante, ut gravida justo dignissim pellentesque. Quisque consectetur tincidunt odio quis condimentum. Vivamus eu turpis diam. Curabitur dignissim lacinia orci non placerat. Aliquam malesuada nunc eu tortor cursus, at auctor tellus viverra.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+@endsection
